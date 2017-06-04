0cd9e11cbba843a66da4270e99fe81b7f39b5202
Marcin Maciukiewicz <mm@bluepocket.com>
2017-06-04 20:19:56 +0100

    Rewrite of rx.getPublicPhotos

--- FlickrBrowser/FlickrBrowser/API/FlickrAPI+rx.swift
+++ FlickrBrowser/FlickrBrowser/API/FlickrAPI+rx.swift
@@ -12,28 +12,50 @@ import RxSwift
 extension Reactive where Base: FlickrAPI {
     
     public var allPublicPhotos: Observable<FlickrFeed> {
-        return publicPhotos(query: PublicPhotosAPIQuery.empty)
+        return getPublicPhotos(query: PublicPhotosAPIQuery.empty)
     }
     
-    public func publicPhotos(query: PublicPhotosAPIQuery) -> Observable<FlickrFeed> {
+    public func getPublicPhotos(query: PublicPhotosAPIQuery) -> Observable<FlickrFeed> {
         
         return Observable.create { (observer: AnyObserver<FlickrFeed>) -> Disposable in
             
-            self.base.getPublicPhotos(query: query, result: { (flickFeed: FlickrFeed) in
-                observer.onNext(flickFeed)
-                observer.onCompleted()
-            }, error: { (error:Error?) in
-                guard let err = error else {
-                    observer.onCompleted()
-                    return
-                }
-                observer.onError(err)
-                observer.onCompleted()
-            })
+            let disposeBag = DisposeBag()
+            Observable
+                .just(query)
+                .map({ (query: PublicPhotosAPIQuery) -> [String:String] in
+                    return query.queryParams
+                })
+                .map({ (queryParams: [String : String]) -> URL in
+                    let endpointURL = self.base.config.createEndpointURL(service: .publicPhotos, queryParams: queryParams)
+                    return endpointURL
+                })
+                .map({ (serviceURL: URL) -> NetworkOperationBlock in
+                    self.base.config.networkProvider.createGETOperation(url: serviceURL, operationResult: { (operationStatus: NetworkOperationStatus) in
+                        FlickrAPI.rx.retrievedPublicPhotos(observer: observer, status: operationStatus)
+                    })
+                })
+                .subscribe(onNext: { (operation: @escaping NetworkOperationBlock) in
+                    self.base.config.networkExecutor.execute(operation: operation)
+                }).disposed(by: disposeBag)
+            
             
             return Disposables.create()
         }
         
     }
-    
+
+    static func retrievedPublicPhotos(observer: AnyObserver<FlickrFeed>, status: NetworkOperationStatus) {
+        switch status {
+        case .successful(let data):
+            if let flickrFeed = FlickrFeed.parse(feedAsJsonData: data) {
+                observer.onNext(flickrFeed)
+            } else {
+                observer.onError(FlickrAPIError.jsonDeserialisationError)
+            }
+        case .failed(let error):
+            let err = error ?? FlickrAPIError.unknownError
+            observer.onError(err)
+            return
+        }
+    }
 }
